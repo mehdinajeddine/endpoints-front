d86108a2c20cfecddf100d405489b462a6e08550
mehdinajeddine <mehdi.najeddine@void.fr>
2022-02-24 21:40:00 +0100

    update model to link people

--- src/components/ModelView.js
+++ src/components/ModelView.js
@@ -13,6 +13,7 @@ const ModelView = () => {
   const [refreshModels, setRefreshModels] = useState(false);
   const { id } = useParams();
   const head = useRef();
+  const [linktopeople, setLinktopeople] = useState(false);
 
   Scroller({ ref: head, isLoading: isLoading });
 
@@ -122,7 +123,7 @@ const ModelView = () => {
     e.preventDefault();
     await axios.post(
       process.env.REACT_APP_HOST + "/model/generate/",
-      { id: id },
+      { id: id, linktopeople: linktopeople },
       {
         headers: {
           Authorization: "Bearer " + cookies.get("token"),
@@ -257,48 +258,85 @@ const ModelView = () => {
                     </li>
                   );
                 })}
-                <form id="form-add-field" onSubmit={handleAddField}>
-                  <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                    <div className="w-0 flex-1 flex items-center">
-                      <span className="ml-2 flex-1 w-0 ">
-                        <input
-                          value={label}
-                          onChange={(e) => {
-                            setLabel(e.target.value);
-                          }}
-                          id="label"
-                          className="appearance-none rounded-none   w-28 px-1 py-1 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-teal-500 focus:border-teal-500 focus:z-10 sm:text-sm"
-                        />
-                        <select
-                          value={type}
-                          onChange={(e) => {
-                            setType(e.target.value);
-                          }}
-                          id="type"
-                          className="form-select appearance-none rounded-none   w-28 px-1 py-1 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-teal-500 focus:border-teal-500 focus:z-10 sm:text-sm"
-                        >
-                          <option value="String">String</option>
-                          <option value="mongoose.Schema.Types.Mixed">
-                            Image
-                          </option>
-                        </select>
-                      </span>
-                    </div>
-                    <div className="ml-4 flex-shrink-0">
-                      <button className="font-medium text-indigo-600 hover:text-indigo-500">
-                        Add field
-                      </button>
-                    </div>
-                  </li>
-                </form>
+                {!data.model.schema && (
+                  <form id="form-add-field" onSubmit={handleAddField}>
+                    <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                      <div className="w-0 flex-1 flex items-center">
+                        <span className="ml-2 flex-1 w-0 ">
+                          <input
+                            value={label}
+                            onChange={(e) => {
+                              setLabel(e.target.value);
+                            }}
+                            id="label"
+                            className="appearance-none rounded-none   w-28 px-1 py-1 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-teal-500 focus:border-teal-500 focus:z-10 sm:text-sm"
+                          />
+                          <select
+                            value={type}
+                            onChange={(e) => {
+                              setType(e.target.value);
+                            }}
+                            id="type"
+                            className="form-select appearance-none rounded-none   w-28 px-1 py-1 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-teal-500 focus:border-teal-500 focus:z-10 sm:text-sm"
+                          >
+                            <option value="String">String</option>
+                            <option value="mongoose.Schema.Types.Mixed">
+                              Image
+                            </option>
+                          </select>
+                        </span>
+                      </div>
+                      <div className="ml-4 flex-shrink-0">
+                        <button className="font-medium text-indigo-600 hover:text-indigo-500">
+                          Add field
+                        </button>
+                      </div>
+                    </li>
+                  </form>
+                )}
               </ul>
             </dd>
           </div>
         </dl>
+
+        <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+          <dt className="text-sm font-medium text-gray-500">
+            Link to people ?
+          </dt>
+          <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+            <div className="relative flex items-start">
+              <div className="flex items-center h-5">
+                <input
+                  disabled={data.model.schema && true}
+                  checked={linktopeople}
+                  onChange={(e) => setLinktopeople(e.target.checked)}
+                  id="auth"
+                  aria-describedby="auth-description"
+                  name="auth"
+                  type="checkbox"
+                  className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+                />
+              </div>
+              <div className="ml-3 text-sm">
+                <label htmlFor="auth" className="font-medium text-gray-700">
+                  Activate people link
+                </label>
+                <p id="auth-description" className="text-gray-500">
+                  This model will be link to your Users
+                </p>
+              </div>
+            </div>
+          </dd>
+        </div>
       </div>
       <button
+        disabled={data.model.schema && true}
         onClick={handleGenerateModel}
-        className="group relative w-full flex justify-center py-2 px-4 border border-black text-sm font-medium rounded-md  bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+        className={
+          "group relative w-full flex justify-center py-2 px-4 border border-black text-sm font-medium rounded-md " +
+          (!data.model.schema &&
+            " bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500")
+        }
       >
         Generate Model
       </button>
